a2be915b82881cd0a3212671fbc5713a16877177
Nephele Agrafiotis <nephele.agrafiotis@gmail.com>
2019-10-24 13:59:14 -0400

    added back button

--- js/source.js
+++ js/source.js
@@ -1,18 +1,19 @@
 $(document).ready(function(){
   var metadata = $.getJSON("triplets.json");
+  const backup = JSON.parse(JSON.stringify(metadata));
   console.log(metadata);
   var i = 0
     // TODO update with checkmark clicks that update 'positive' field in json
     $('#image1').on({
         'click': function(){
             $('#check1').attr('src','https://s3.amazonaws.com/pix.iemoji.com/images/emoji/apple/ios-12/256/white-heavy-check-mark.png');
-            metadata.responseJSON[i]['Positive'] = '1';
+            metadata.responseJSON[i]['Positive'] = 1;
         }
     });
     $('#image2').on({
         'click': function(){
             $('#check2').attr('src','https://s3.amazonaws.com/pix.iemoji.com/images/emoji/apple/ios-12/256/white-heavy-check-mark.png');
-            metadata.responseJSON[i]['Positive'] = '2';
+            metadata.responseJSON[i]['Positive'] = 2;
         }
     });
     $('#clear').on({
@@ -22,9 +23,9 @@ $(document).ready(function(){
             metadata.responseJSON[i]['Positive'] = "";
         }
     });
-    $('#next').on({
+    $('#back').on({
         'click': function(){
-            i += 1;
+            i -= 1;
             //console.log(metadata);
             $('#anchor').attr('src','images/' + metadata.responseJSON[i]['anchor']);
             $('#image1').attr('src','images/' + metadata.responseJSON[i]['1']);
@@ -33,7 +34,16 @@ $(document).ready(function(){
             $('#check2').attr('src','https://listimg.pinclipart.com/picdir/s/176-1766362_red-x-cross-gif-clipart.png');
         }
     });
+    $('#next').on({
+        'click': function(){
+            i += 1;
+            console.log(metadata);
+            $('#anchor').attr('src','images/' + metadata.responseJSON[i]['anchor']);
+            $('#image1').attr('src','images/' + metadata.responseJSON[i]['1']);
+            $('#image2').attr('src','images/' + metadata.responseJSON[i]['2']);
+            $('#check1').attr('src','https://listimg.pinclipart.com/picdir/s/176-1766362_red-x-cross-gif-clipart.png');
+            $('#check2').attr('src','https://listimg.pinclipart.com/picdir/s/176-1766362_red-x-cross-gif-clipart.png');
+        }
+    });
     // TODO update so that comments are saved in 'notes' in JSON
-    // TODO update so that image triplets are displayed and "next" 
-    // moves to the next triplets
 });
