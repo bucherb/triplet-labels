24369cc64f1503e7d80a72270cdde04406d2f06f
Bernadette Kathleen Bucher <bucherb@seas.upenn.edu>
2019-10-24 01:14:07 -0400

    got interactivate image loading working

--- js/source.js
+++ js/source.js
@@ -1,8 +1,23 @@
 $(document).ready(function(){
   var metadata = $.getJSON("triplets.json");
   console.log(metadata);
-    //    document.getElementById("jsonObject").innerHTML = metadata;
-
+    // TODO update with checkmark clicks that update 'positive' field in json
+    $('#anchor').on({
+        'click': function(){
+            $('#anchor').attr('src','images/0000000.jpeg');
+        }
+    });
+    $('#image1').on({
+        'click': function(){
+            $('#image1').attr('src','images/0000000.jpeg');
+        }
+    });
+    $('#image2').on({
+        'click': function(){
+            $('#image2').attr('src','images/0000000.jpeg');
+        }
+    });
+    // TODO update so that comments are saved in 'notes' in JSON
+    // TODO update so that image triplets are displayed and "next" 
+    // moves to the next triplets
 });
-
-      //$("#fileUploader").onload(function(evt){
