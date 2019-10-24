bbf2d6f2a235718d861a7623c8f608ef5345805b
Bernadette Kathleen Bucher <bucherb@seas.upenn.edu>
2019-10-24 00:59:21 -0400

    added json

--- js/source.js
+++ js/source.js
@@ -1,23 +1,8 @@
-var metadata;
 $(document).ready(function(){
-  var selectedFile = $.get("TRIPLETS.xlsx");
-  var reader = new FileReader();
-  reader.onload = function(event) {
-    var data = event.target.result;
-    var workbook = XLSX.read(data, {
-        type: 'binary'
-    });
-    workbook.SheetNames.forEach(function(sheetName) {
+  var metadata = $.getJSON("triplets.json");
+  console.log(metadata);
+    //    document.getElementById("jsonObject").innerHTML = metadata;
 
-        var XL_row_object = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName]);
-        metadata = JSON.stringify(XL_row_object);
-        document.getElementById("jsonObject").innerHTML = metadata;
-      })
-  };
-  reader.onerror = function(event) {
-    console.error("File could not be read! Code " + event.target.error.code);
-  };
-  reader.readAsBinaryString(selectedFile);
 });
 
       //$("#fileUploader").onload(function(evt){
