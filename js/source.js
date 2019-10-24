d869e24be4883fe5db3cc67635e5166353ffe240
Nephele Agrafiotis <nephele.agrafiotis@gmail.com>
2019-10-24 13:33:15 -0400

    fixed metadata.responseJSON error

--- js/source.js
+++ js/source.js
@@ -12,14 +12,14 @@ $(document).ready(function(){
     $('#image2').on({
         'click': function(){
             $('#check2').attr('src','https://s3.amazonaws.com/pix.iemoji.com/images/emoji/apple/ios-12/256/white-heavy-check-mark.png');
-            metadata[i]['Positive'] = metadata.responseJSON[i]['2'];
+            metadata.responseJSON[i]['Positive'] = metadata.responseJSON[i]['2'];
         }
     });
     $('#clear').on({
         'click': function(){
             $('#check1').attr('src','https://listimg.pinclipart.com/picdir/s/176-1766362_red-x-cross-gif-clipart.png');
             $('#check2').attr('src','https://listimg.pinclipart.com/picdir/s/176-1766362_red-x-cross-gif-clipart.png');
-            metadata[i]['Positive'] = "";
+            metadata.responseJSON[i]['Positive'] = "";
         }
     });
     $('#next').on({
@@ -29,15 +29,6 @@ $(document).ready(function(){
             $('#anchor').attr('src','images/' + metadata.responseJSON[i]['anchor']);
             $('#image1').attr('src','images/' + metadata.responseJSON[i]['1']);
             $('#image2').attr('src','images/' + metadata.responseJSON[i]['2']);
-            //document.getElementById('anchor').src = 'images/' + metadata[i]['anchor'];
-            //document.getElementById('image1').src = 'images/' + metadata[i]['1'];
-            //document.getElementById('image2').src = 'images/' + metadata[i]['2'];
-            //var anc = 'images/' + metadata[i]['anchor'];
-            //var one = 'images/' + metadata[i]['1'];
-            //var two = 'images/' + metadata[i]['2'];
-            //document.getElementById('anchor').src = anc;
-            //document.getElementById('image1').src = one;
-            //document.getElementById('image2').src = two;
             $('#check1').attr('src','https://listimg.pinclipart.com/picdir/s/176-1766362_red-x-cross-gif-clipart.png');
             $('#check2').attr('src','https://listimg.pinclipart.com/picdir/s/176-1766362_red-x-cross-gif-clipart.png');
         }
